6dd05f9fbf688ee0081dedfb8bfd4a40b65fd929
Mateus Bazani <mrbazani@gmail.com>
2022-02-07 00:06:30 -0300

    feat: add logs da geracao do banco

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,15 +4,23 @@ class Db {
     private db: any;
 
     constructor () {
+        console.log('Conectando ao banco ...');
         this.db = connect('banco_do_banco.db'); 
     }
 
     public async prepare() {
-        await this.createDb();
-        await this.populateDb();
+        try {
+            await this.createDb();
+            await this.populateDb();
+        } catch (err) {
+            console.error('Whopsie', err);
+            console.error('Finalizando api, erro ao conectaro ao banco.');
+            process.exit(1);
+        }
     }
 
     private async createDb() {
+        console.log('Verificando estrutura ...');
         await this.db.query(sql`
             CREATE TABLE IF NOT EXISTS extrato(
                 id INTEGER PRIMARY KEY,
@@ -33,6 +41,7 @@ class Db {
         `);
 
         if (!result.length) {
+            console.log('Gerando massa para testes ...');
             await this.db.query(sql`INSERT INTO extrato VALUES(1, '89e', '2022-01-01', 'D', 10, '', 135.55, 293210.23)`);
         }
     }
